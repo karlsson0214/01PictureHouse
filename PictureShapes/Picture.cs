11b3ad75a42d5e4a014d639553689707f2d12950
Karlsson <karlsson0214@gmail.com>
2019-09-08 22:31:34 +0200

    Add project files.

---
+++ PictureShapes/Picture.cs
@@ -0,0 +1,23 @@
+﻿namespace PictureShapes
+{
+    class Picture
+    {
+
+        public Picture()
+        {
+            DrawImage();
+        }
+        public void DrawImage()
+        {
+            Square wall = new Square();
+            wall.MakeVisible();
+            wall.ChangeColor("red");
+            wall.ChangeSize(200);
+            wall.MoveHorizontal(70);
+            wall.MoveVertical(20);
+
+            // Write your code here
+
+        }
+    }
+}
